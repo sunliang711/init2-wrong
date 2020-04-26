0281fe82f324f7401727f44a2bb755da8f82afdd
sunliang <sunliang711@163.com>
2020-04-26 15:29:25 +0800

    add -h -H option for fcd fe

--- tools/fzffunctions.sh
+++ tools/fzffunctions.sh
@@ -3,6 +3,7 @@ if command -v fd >/dev/null 2>&1;then
 fi
 export FZF_DEFAULT_OPTS='-m --height 70% --reverse --border'
 
+##TODO add -h -H like fcd
 fzff(){
     local dest=${1:-$HOME}
     if [ -d "$dest" ];then
@@ -16,6 +17,7 @@ fzff(){
     fd --type f | fzf || { echo "canceld"; cd $wd; }
 }
 
+##TODO add -h -H like fcd
 fzfd(){
     local dest=${1:-$HOME}
     if [ -d "$dest" ];then
@@ -34,6 +36,27 @@ fp() {
 }
 
 fe(){
+    fdoption="--type f"
+    findoption="-type f -not -path '*/\.*'"
+    while getopts ":hH" opt;do
+        case "$opt" in
+            h)
+                cat<<-EOF
+				Usage: $0 [Options] [path (default: \$HOME)]
+				Options:
+				-h          help
+				-H          show hidden files
+EOF
+                return
+                ;;
+            H)
+                fdoption="-HI --type f"
+                findoption="-type f"
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
+
     local dest=${1:-$HOME}
     if [ -d "$dest" ];then
         local wd=$(pwd)
@@ -53,9 +76,9 @@ fe(){
     fi
 
     if command -v fd >/dev/null 2>&1;then
-        file=$(fd --type f | fzf --border --height 60% --reverse -m --bind 'ctrl-f:preview-page-down' --bind 'ctrl-b:preview-page-up' --preview '[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || rougify {}  || highlight -O ansi -l {} || coderay {} || cat -n {}) 2> /dev/null | head -500')
+        file=$(eval "fd $fdoption" | fzf --border --height 60% --reverse -m --bind 'ctrl-f:preview-page-down' --bind 'ctrl-b:preview-page-up' --preview '[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || rougify {}  || highlight -O ansi -l {} || coderay {} || cat -n {}) 2> /dev/null | head -500')
     else
-        file=$(find . -not -path '*/\.*' -type f | fzf --border --height 60% --reverse -m --bind 'ctrl-f:preview-page-down' --bind 'ctrl-b:preview-page-up' --preview '[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || rougify {}  || highlight -O ansi -l {} || coderay {} || cat -n {}) 2> /dev/null | head -500')
+        file=$(eval "find . $findoption" | fzf --border --height 60% --reverse -m --bind 'ctrl-f:preview-page-down' --bind 'ctrl-b:preview-page-up' --preview '[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || rougify {}  || highlight -O ansi -l {} || coderay {} || cat -n {}) 2> /dev/null | head -500')
     fi
     if [ -n "$file" ];then
         # multi files
@@ -64,6 +87,65 @@ fe(){
     cd "$wd"
 }
 
+fcd(){
+    fdoption="--type d"
+    findoption="-type d -not -path '*/\.*'"
+    while getopts ":hH" opt;do
+        case "$opt" in
+            h)
+                cat<<-EOF
+				Usage: $0 [Options] [path(default: \$HOME)]
+				Options:
+				-h              help
+				-H              show hidden direcories
+				EOF
+                return 0
+                ;;
+            H)
+                fdoption="-HI --type d"
+                findoption="-type d"
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
+
+    local dest=${1:-$HOME}
+    if [ -d "$dest" ];then
+        local wd=$(pwd)
+        cd "$dest"
+    else
+        echo "No such directory: '$dest'"
+        return 1
+    fi
+
+    local dir
+    if command -v fd >/dev/null 2>&1;then
+        dir=$(eval "fd ${fdoption}" | fzf --height 60% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
+    else
+        dir=$(eval "find . ${findoption}" | fzf --height 60% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
+    fi
+}
+
+## deprecated
+fCD(){
+    local dest=${1:-$HOME}
+    if [ -d "$dest" ];then
+        local wd=$(pwd)
+        cd "$dest"
+    else
+        echo "No such directory: '$dest'"
+        return 1
+    fi
+
+    local  dir
+    if command -v fd >/dev/null 2>&1;then
+        dir=$(fd -HI --type d | fzf --height 80% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
+    else
+        dir=$(find . -type d | fzf --height 80% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
+    fi
+}
+## deprecated
+## deprecated
 fE(){
     local dest=${1:-$HOME}
     if [ -d "$dest" ];then
@@ -94,39 +176,4 @@ fE(){
     fi
     cd "$wd"
 }
-
-fcd(){
-    local dest=${1:-$HOME}
-    if [ -d "$dest" ];then
-        local wd=$(pwd)
-        cd "$dest"
-    else
-        echo "No such directory: '$dest'"
-        return 1
-    fi
-
-    local dir
-    if command -v fd >/dev/null 2>&1;then
-        dir=$(fd --type d | fzf --height 60% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
-    else
-        dir=$(find . -not -path '*/\.*' | fzf --height 60% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
-    fi
-}
-
-fCD(){
-    local dest=${1:-$HOME}
-    if [ -d "$dest" ];then
-        local wd=$(pwd)
-        cd "$dest"
-    else
-        echo "No such directory: '$dest'"
-        return 1
-    fi
-
-    local  dir
-    if command -v fd >/dev/null 2>&1;then
-        dir=$(fd -HI --type d | fzf --height 80% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
-    else
-        dir=$(find . -type d | fzf --height 80% --border --reverse) && cd "$dir" || { echo "Canceled"; cd "$wd"; }
-    fi
-}
+## deprecated
