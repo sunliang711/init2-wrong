75137d5f0a5e9af69d0c05691f36def982b51c64
sunliang <sunliang711@163.com>
2020-09-07 13:07:57 +0800

    add script installNodejs.sh

---
+++ tools/installNodejs.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+rpath="$(readlink ${BASH_SOURCE})"
+if [ -z "$rpath" ];then
+    rpath=${BASH_SOURCE}
+fi
+thisDir="$(cd $(dirname $rpath) && pwd)"
+cd "$thisDir"
+
+user="${SUDO_USER:-$(whoami)}"
+home="$(eval echo ~$user)"
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+cyan=$(tput setaf 5)
+        bold=$(tput bold)
+reset=$(tput sgr0)
+function runAsRoot(){
+    verbose=0
+    while getopts ":v" opt;do
+        case "$opt" in
+            v)
+                verbose=1
+                ;;
+            \?)
+                echo "Unknown option: \"$OPTARG\""
+                exit 1
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
+    cmd="$@"
+    if [ -z "$cmd" ];then
+        echo "${red}Need cmd${reset}"
+        exit 1
+    fi
+
+    if [ "$verbose" -eq 1 ];then
+        echo "run cmd:\"${red}$cmd${reset}\" as root."
+    fi
+
+    if (($EUID==0));then
+        sh -c "$cmd"
+    else
+        if ! command -v sudo >/dev/null 2>&1;then
+            echo "Need sudo cmd"
+            exit 1
+        fi
+        sudo sh -c "$cmd"
+    fi
+}
+###############################################################################
+# write your code below (just define function[s])
+# function with 'function' is hidden when run help, without 'function' is show
+###############################################################################
+# TODO
+version=12.16.1
+    https://source711.oss-cn-shanghai.aliyuncs.com/nodejs/12.16.1/node-v12.16.1-linux-x64.tar.xz
+install(){
+    dest=$HOME/.app/nodejs/$version
+    if [ ! -d $dest ];then
+        mkdir -p $dest
+    fi
+    case $(uname) in
+        Linux)
+            local nodeURL=https://source711.oss-cn-shanghai.aliyuncs.com/nodejs/${version}/node-v${version}-linux-x64.tar.xz
+            local name=node-v{version-linux-x64}
+            ;;
+        Darwin)
+            echo "not support."
+            exit 1
+            ;;
+    esac
+
+    cd /tmp
+    if [ ! -e ${name}.tar.xz ];then
+        echo "Download $name.tar.xz to /tmp..."
+        curl -LO $nodeURL || { echo "Download error!"; exit 1; }
+    fi
+
+    tar -C $dest -xvf $name && echo "nodejs has been installed to $dest add $dest/name/bin to PATH manually."; 
+    cd -
+}
+
+uninstall(){
+    dest=$HOME/.app/nodejs/$version
+    if [ -d $dest ];then
+        echo "Remove $dest"
+        /bin/rm -rf $dest && echo "Done."
+    fi
+
+}
+
+
+
+###############################################################################
+# write your code above
+###############################################################################
+function help(){
+    cat<<EOF2
+Usage: $(basename $0) ${bold}CMD${reset}
+
+${bold}CMD${reset}:
+EOF2
+    perl -lne 'print "\t$1" if /^\s*(\w+)\(\)\{$/' $(basename ${BASH_SOURCE}) | grep -v runAsRoot
+}
+function loadENV(){
+    if [ -z "$INIT_HTTP_PROXY" ];then
+        echo "INIT_HTTP_PROXY is empty"
+        echo -n "Enter http proxy: (if you need) "
+        read INIT_HTTP_PROXY
+    fi
+    if [ -n "$INIT_HTTP_PROXY" ];then
+        echo "set http proxy to $INIT_HTTP_PROXY"
+        export http_proxy=$INIT_HTTP_PROXY
+        export https_proxy=$INIT_HTTP_PROXY
+        export HTTP_PROXY=$INIT_HTTP_PROXY
+        export HTTPS_PROXY=$INIT_HTTP_PROXY
+        git config --global http.proxy $INIT_HTTP_PROXY
+        git config --global https.proxy $INIT_HTTP_PROXY
+    else
+        echo "No use http proxy"
+    fi
+}
+
+function unloadENV(){
+    if [ -n "$https_proxy" ];then
+        unset http_proxy
+        unset https_proxy
+        unset HTTP_PROXY
+        unset HTTPS_PROXY
+        git config --global --unset-all http.proxy
+        git config --global --unset-all https.proxy
+    fi
+}
+
+
+case "$1" in
+     ""|-h|--help|help)
+        help
+        ;;
+    *)
+        "$@"
+esac
